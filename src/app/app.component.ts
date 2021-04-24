87366785fc84cdbe4f1a8a19d9dd842cf136f088
Ishika-D <ishikadawda2000@gmail.com>
2021-04-24 12:10:19 +0530

    Trying to commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title="Notes-app";
   notes:string[] = [];
   i=0;
   constructor() {
